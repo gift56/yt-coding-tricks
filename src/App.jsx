c63a1ca8ca1999bb05827ffc97f62da8affcf6da
gift56 <efeasiughu@gmail.com>
2023-07-24 13:16:50 -0700

    done with first project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useRef, useEffect } from "react";
+import { motion } from "framer-motion";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [show, setShow] = useState(false);
+  const modalRef = useRef();
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current?.contains(event.target)) {
+        setShow(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [show]);
+
+  const varaint = {
+    open: { opacity: 1, scale: 1 },
+    closed: { opacity: 0, scale: 0 },
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="w-full flex items-center flex-col justify-center h-screen overflow-hidden bg-gray-400 text-black">
+      <button
+        onClick={() => setShow(true)}
+        className="w-fit p-3 rounded-lg font-medium bg-white cursor-pointer outline-none"
+      >
+        Click Me
+      </button>
 
-export default App
+      <div
+        className={`fixed top-0 right-0 w-full h-full bg-[#0000004f] z-40 place-items-center flex justify-center transition-all duration-500 overflow-auto ${
+          show ? "flex" : "hidden"
+        }`}
+      >
+        <motion.div
+          animate={show ? "open" : "closed"}
+          variants={varaint}
+          ref={modalRef}
+          className="w-[90%] md:w-[600px] p-4 bg-white rounded-lg flex flex-col items-start justify-start h-fit"
+        >
+          <h2>Opened</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit,
+            asperiores tenetur. Numquam eligendi, hic repellat aperiam ab soluta
+            rerum excepturi.
+          </p>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
