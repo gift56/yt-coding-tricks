7a00c8c8d87e540c7b6a7ad48aae7fa31cd8466b
gift56 <efeasiughu@gmail.com>
2023-09-23 12:03:58 -0400

    working on recaptcha

--- src/components/Recaptcha.jsx
+++ src/components/Recaptcha.jsx
@@ -5,11 +5,10 @@ const RecaptchaComponent = () => {
   const [verified, setVerified] = useState(false);
 
   function handleSubscripe() {
-    if(verified){
-        alert("You have Successfully subscribed!")
-    }
-    else{
-        alert("Please verify if you're a human")
+    if (verified) {
+      alert("You have Successfully subscribed!");
+    } else {
+      alert("Please verify if you're a human");
     }
   }
 
@@ -26,6 +25,10 @@ const RecaptchaComponent = () => {
       >
         Subscribe
       </button>
+      <ReCAPTCHA
+        sitekey="6Lc5b0ooAAAAAFV6RuPHVkfNIs4TSH3gI5CYTGdf"
+        onChange={onChange}
+      />
     </div>
   );
 };
