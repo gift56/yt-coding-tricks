10c37684a55ff05ecb79aef43c2c2b0b66446374
gift56 <efeasiughu@gmail.com>
2023-09-23 11:47:09 -0400

    working on recaptcha

--- src/components/Recaptcha.jsx
+++ src/components/Recaptcha.jsx
@@ -1,5 +1,7 @@
-const Recaptcha = () => {
+import ReCAPTCHA from "react-google-recaptcha";
+
+const RecaptchaComponent = () => {
   return <div>Recaptcha</div>;
 };
 
-export default Recaptcha;
+export default RecaptchaComponent;
