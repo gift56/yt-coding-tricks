35ca6b0319c4c181c1b58448f3b9f2af79ede568
gift56 <efeasiughu@gmail.com>
2023-09-23 12:01:13 -0400

    working on recaptcha

--- src/components/Recaptcha.jsx
+++ src/components/Recaptcha.jsx
@@ -3,14 +3,22 @@ import ReCAPTCHA from "react-google-recaptcha";
 
 const RecaptchaComponent = () => {
   const [verified, setVerified] = useState(false);
-  function handleSubscript(value) {
-    console.log("Captcha value:", value);
-  }
+
+  function handleSubscripe() {}
 
   return (
     <div>
-      <input type="text" placeholder="Email Address" className="w-[400px] bg-white rounded outline-none h-14" />
-      <button></button>
+      <input
+        type="text"
+        placeholder="Email Address"
+        className="w-[400px] bg-white rounded outline-none h-14 px-4"
+      />
+      <button
+        onClick={handleSubscripe}
+        className="px-4 h-14 bg-purple-700 text-white text-base font-semibold"
+      >
+        Subscribe
+      </button>
     </div>
   );
 };
