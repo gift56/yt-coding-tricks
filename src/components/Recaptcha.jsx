bfa2342f4dbea255096400925e0f5d48ebb26bab
gift56 <efeasiughu@gmail.com>
2023-09-23 11:48:00 -0400

    working on recaptcha

--- src/components/Recaptcha.jsx
+++ src/components/Recaptcha.jsx
@@ -1,7 +1,10 @@
 import ReCAPTCHA from "react-google-recaptcha";
 
 const RecaptchaComponent = () => {
-  return <div>Recaptcha</div>;
+  function onChange(value) {
+    console.log("Captcha value:", value);
+  }
+  return <ReCAPTCHA sitekey="6Lc5b0ooAAAAAFV6RuPHVkfNIs4TSH3gI5CYTGdf" onChange={onChange} />;
 };
 
 export default RecaptchaComponent;
